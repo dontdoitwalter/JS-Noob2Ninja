ab68684abce463c7c29b9666183d3e31571c9f41
dontdoitwalter <emailwaltervanhooser@gmail.com>
2019-06-10 17:31:02 -0400

    another commit

--- ModernJS/chapter_7/formsAndFormEvents.js
+++ ModernJS/chapter_7/formsAndFormEvents.js
@@ -4,11 +4,39 @@ SUBMIT EVENTS
 ***************************************************************************************************************************
 **************************************************************************************************************************/
 
+// const form = document.querySelector('.signup-form');
+// //const username = document.querySelector('#username');
+
+// form.addEventListener('submit', e => {
+//     e.preventDefault();
+//     //console.log(username.value);
+//     console.log(form.username.value)
+// });
+
+/*************************************************************************************************************************
+**************************************************************************************************************************
+REGULAR EXPRESSIONS
+***************************************************************************************************************************
+**************************************************************************************************************************/
+
 const form = document.querySelector('.signup-form');
-//const username = document.querySelector('#username');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    //console.log(username.value);
-    console.log(form.username.value)
+    console.log(form.username.value);
 });
+
+//testing RegEx
+const username = 'shaunshaun';
+const pattern = /^[a-z]{6,}$/
+
+// let result = pattern.test(username);
+// console.log(result);
+// if(result){
+//     console.log('regex test passed :)')
+// } else{
+//     console.log('regex test failed :(')
+// }
+
+let result = username.search(pattern);
+console.log(result)
