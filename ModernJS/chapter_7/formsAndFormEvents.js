1a3b40d563682e3a15fd1b2cfcfc72ae2ac7f7cd
dontdoitwalter <emailwaltervanhooser@gmail.com>
2019-06-19 16:48:49 -0400

    more progress

--- ModernJS/chapter_7/formsAndFormEvents.js
+++ ModernJS/chapter_7/formsAndFormEvents.js
@@ -19,24 +19,82 @@ REGULAR EXPRESSIONS
 ***************************************************************************************************************************
 **************************************************************************************************************************/
 
-const form = document.querySelector('.signup-form');
+// const form = document.querySelector('.signup-form');
 
-form.addEventListener('submit', e => {
+// form.addEventListener('submit', e => {
+//     e.preventDefault();
+//     console.log(form.username.value);
+// });
+
+// //testing RegEx
+// const username = 'shaunshaun';
+// const pattern = /^[a-z]{6,}$/
+
+// // let result = pattern.test(username);
+// // console.log(result);
+// // if(result){
+// //     console.log('regex test passed :)')
+// // } else{
+// //     console.log('regex test failed :(')
+// // }
+
+// let result = username.search(pattern);
+// console.log(result)
+
+/*************************************************************************************************************************
+**************************************************************************************************************************
+FORM VALIDATION
+***************************************************************************************************************************
+**************************************************************************************************************************/
+
+// const  form = document.querySelector('.signup-form');
+// const feedback = document.querySelector('.feedback')
+
+// form.addEventListener('submit', e =>{
+//     e.preventDefault();
+//         //validation
+//     const username = form.username.value;
+//     const usernamePattern = /^[a-zA-Z]{6, 12}$/;
+
+//     if(usernamePattern.test(username)){
+//         //positive feedback
+//         feedback.textContent = 'valid username';
+//     } else {
+//         //negative feedback
+//         feedback.textContent = 'username is invalid';
+//     }
+// })
+
+/*************************************************************************************************************************
+**************************************************************************************************************************
+KEYBOARD EVENTS
+***************************************************************************************************************************
+**************************************************************************************************************************/
+
+const  form = document.querySelector('.signup-form');
+const feedback = document.querySelector('.feedback');
+const usernamePattern = /^[a-zA-Z]{6,12}$/;
+
+
+form.addEventListener('submit', e =>{
     e.preventDefault();
-    console.log(form.username.value);
+        //validation
+    const username = form.username.value;
+
+    if(usernamePattern.test(username)){
+        //positive feedback
+        feedback.textContent = 'valid username';
+    } else {
+        //negative feedback
+        feedback.textContent = 'username is invalid';
+    }
 });
 
-//testing RegEx
-const username = 'shaunshaun';
-const pattern = /^[a-z]{6,}$/
-
-// let result = pattern.test(username);
-// console.log(result);
-// if(result){
-//     console.log('regex test passed :)')
-// } else{
-//     console.log('regex test failed :(')
-// }
-
-let result = username.search(pattern);
-console.log(result)
+//LIVE FEEDBACK
+form.username.addEventListener('keyup', e => {
+    if(usernamePattern.test(e.target.value)){
+        form.username.setAttribute('class','success');
+    } else{
+        form.username.setAttribute('class','error');
+    }
+});
