845acc06a2e38eaca6765d48f6b42b1b33cd1af8
Walter Van Hooser <emailwaltervanhooser@gmail.com>
2019-06-25 11:36:03 -0400

    more work

--- ModernJS/chapter_13/scripts/app.js
+++ ModernJS/chapter_13/scripts/app.js
@@ -1,17 +1,58 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateUI = (data) => {
+
+    // const cityDets = data.cityDets;
+    // const weather = data.weather;       //these are the same as line 11
+
+    //destructuring properties
+    const {cityDets, weather} = data;
+
+    //update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+        <span>${weather.Temperature.Imperial.Value}</span>
+        <span>&deg;F</span>
+    `;
+
+    //update the night/day images & icon
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    // let timeSrc = null;
+    // if(weather.IsDayTime){
+    //     timeSrc = 'img/day.svg';
+    // } else {
+    //     timeSrc = 'img/night.svg'
+    // }
+
+    //ternary version of lines 28-33
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg'
+
+    time.setAttribute('src', timeSrc);
+
+    
+    //remove d-none class if present
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    };
+}
 
 const updateCity = async (city) => {
     const cityDets = await getCity(city);
     const weather = await getWeather(cityDets.Key);
 
-    return {
-        cityDets: cityDets,
-        weather: weather
-    };
+    return {cityDets,weather};
 }
 
 cityForm.addEventListener('submit', e => {
-    //prevent defaulkt action
+    //prevent default action
     e.preventDefault();
 
     //get city value
@@ -20,6 +61,15 @@ cityForm.addEventListener('submit', e => {
 
     //update ui with new city
     updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
-})
+
+    //set local storage
+    localStorage.setItem('city', city)
+});
+
+if(localStorage.getItem('city')){
+    updateCity(localStorage.getItem('city'))
+    .then(data => updateUI(data))
+    .catch(err => console.log(err))
+}
