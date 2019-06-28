635b72a0474bfd1d83274527ae9075ac2a665a19
Walter Van Hooser <emailwaltervanhooser@gmail.com>
2019-06-28 14:15:51 -0400

    databases start

--- ModernJS/15_OOPJS/OOP.js
+++ ModernJS/15_OOPJS/OOP.js
@@ -112,43 +112,107 @@ SUPER()
 ***************************************************************************************************************************
 **************************************************************************************************************************/
 
-class User {
+// class User {
 
-    constructor(username, email){
-        //set properties
-        this.username = username;
-        this.email = email;
-        this.score = 0;
-    }
-    login(){
-        console.log(`${this.username} just logged in`);
-        return this;
-    }
-    logout(){
-        console.log(`${this.username} just logged out`);
-        return this;
-    }
-    incScore(){
-        this.score += 1;
-        console.log(`${this.username} has a score of ${this.score}`);
-        return this;
-    }
-}
+//     constructor(username, email){
+//         //set properties
+//         this.username = username;
+//         this.email = email;
+//         this.score = 0;
+//     }
+//     login(){
+//         console.log(`${this.username} just logged in`);
+//         return this;
+//     }
+//     logout(){
+//         console.log(`${this.username} just logged out`);
+//         return this;
+//     }
+//     incScore(){
+//         this.score += 1;
+//         console.log(`${this.username} has a score of ${this.score}`);
+//         return this;
+//     }
+// }
 
-class Admin extends User{
-    constructor(username, email, title){
-        super(username, email)
-        this.title = title;
-    }
-    deleteUser(user){
-        users = users.filter((u) => {
-            return u.username !== user.username
-        })
-    }
-}
+// class Admin extends User{
+//     constructor(username, email, title){
+//         super(username, email)
+//         this.title = title;
+//     }
+//     deleteUser(user){
+//         users = users.filter((u) => {
+//             return u.username !== user.username
+//         })
+//     }
+// }
 
-const userOne = new User('mario', 'mario@thenetninja.co.uk');
-const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
-const userThree = new Admin('shaun','shaun@thenetninja.co.uk', 'black-belt-ninja');
+// const userOne = new User('mario', 'mario@thenetninja.co.uk');
+// const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
+// const userThree = new Admin('shaun','shaun@thenetninja.co.uk', 'black-belt-ninja');
 
-console.log(userThree)
+// console.log(userThree)
+
+/*************************************************************************************************************************
+**************************************************************************************************************************
+PROTOTYPE MODEL     (before the class keyword)
+***************************************************************************************************************************
+**************************************************************************************************************************/
+
+// function User(username, email){
+//     this.username = username;
+//     this.email = email;
+// }
+
+// User.prototype.login = function (){
+//     console.log(`${this.username} has logged in`);
+//     return this;
+// }
+// User.prototype.logout = function (){
+//     console.log(`${this.username} has logged out`);
+//     return this;
+// }
+
+// const userOne = new User('mario','mario@thenetninja.co.uk');
+// const userTwo = new User('luigi','luigi@thenetninja.co.uk');
+
+// console.log(userOne, userTwo);
+// userOne.login().logout();
+
+/*************************************************************************************************************************
+**************************************************************************************************************************
+PROTOTYPAL INHERITANCE
+***************************************************************************************************************************
+**************************************************************************************************************************/
+
+// function User(username, email){
+//     this.username = username;
+//     this.email = email;
+// }
+
+// User.prototype.login = function (){
+//     console.log(`${this.username} has logged in`);
+//     return this;
+// }
+// User.prototype.logout = function (){
+//     console.log(`${this.username} has logged out`);
+//     return this;
+// }
+
+// function Admin(username, email, title){
+//     User.call(this, username, email);
+//     this.title = title
+// }
+
+// Admin.prototype = Object.create(User.prototype);
+
+// Admin.prototype.deleteUser = function(){
+//     //delete user
+// }
+
+// const userOne = new User('mario','mario@thenetninja.co.uk');
+// const userTwo = new User('luigi','luigi@thenetninja.co.uk');
+// const userThree = new Admin('shaun', 'shaun@thenetninja.co.uk','black belt ninja')
+
+// console.log(userOne, userTwo, userThree);
+// userOne.login().logout();
