ce55e0703818c226c7a3a5a833cdfd6aa04b1957
dontdoitwalter <emailwaltervanhooser@gmail.com>
2019-06-28 09:34:38 -0400

    oops

---
+++ ModernJS/15_OOPJS/OOP.js
@@ -0,0 +1,154 @@
+/*************************************************************************************************************************
+**************************************************************************************************************************
+OBJECT LITERAL RECAP
+***************************************************************************************************************************
+**************************************************************************************************************************/
+
+// const  userOne = {
+//     username: 'ryu',
+//     email:'ryu@thenetninja.co.uk',
+//     login(){
+//         console.log('the user logged in')
+//     },
+//     logout(){
+//         console.log('the user logged out')
+//     }
+// };
+
+// console.log(userOne.email, userOne.username);
+// userOne.login();
+
+// const userTwo = {
+//     username: 'Chun Li',
+//     email: 'chun.li@thenetninja.co.uk',
+//     login(){
+//         console.log('the user logged in')
+//     },
+//     logout(){
+//         console.log('the user logged out')
+//     }
+// }
+// console.log(userTwo.email, userTwo.username);
+// userTwo.login();
+
+// //this is what we want to do
+// const userThree = new userOne('shaun@thenetninja.co.uk', 'shaun')
+
+/*************************************************************************************************************************
+**************************************************************************************************************************
+CLASS CONSTRUCTORS
+***************************************************************************************************************************
+**************************************************************************************************************************/
+
+// class User {
+//     constructor(username, email){
+//         //set properties
+//         this.username = username;
+//         this.email = email;
+//     }
+// }
+
+// const userOne = new User('mario', 'mario@thenetninja.co.uk');
+// const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
+
+// console.log(userOne, userTwo);
+
+/*************************************************************************************************************************
+**************************************************************************************************************************
+CLASS METHODS & METHOD CHAINING & CLASS INHERITANCE(subclasses)
+***************************************************************************************************************************
+**************************************************************************************************************************/
+
+// class User {
+
+//     constructor(username, email){
+//         //set properties
+//         this.username = username;
+//         this.email = email;
+//         this.score = 0;
+//     }
+//     login(){
+//         console.log(`${this.username} just logged in`);
+//         return this;
+//     }
+//     logout(){
+//         console.log(`${this.username} just logged out`);
+//         return this;
+//     }
+//     incScore(){
+//         this.score += 1;
+//         console.log(`${this.username} has a score of ${this.score}`);
+//         return this;
+//     }
+// }
+
+// class Admin extends User{
+//     deleteUser(user){
+//         users = users.filter((u) => {
+//             return u.username !== user.username
+//         })
+//     }
+// }
+
+// const userOne = new User('mario', 'mario@thenetninja.co.uk');
+// const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
+// const userThree = new Admin('shaun','shaun@thenetninja.co.uk')
+
+// console.log(userOne, userTwo, userThree);
+// let users = [userOne, userTwo, userThree];
+// console.log(users)
+
+// userThree.deleteUser(userTwo)
+// // userOne.login().incScore().incScore().logout();
+// // userOne.logout();
+// // userTwo.login();
+// // userTwo.logout();
+// // userOne.incScore().incScore();
+// console.log(users)
+
+/*************************************************************************************************************************
+**************************************************************************************************************************
+SUPER()
+***************************************************************************************************************************
+**************************************************************************************************************************/
+
+class User {
+
+    constructor(username, email){
+        //set properties
+        this.username = username;
+        this.email = email;
+        this.score = 0;
+    }
+    login(){
+        console.log(`${this.username} just logged in`);
+        return this;
+    }
+    logout(){
+        console.log(`${this.username} just logged out`);
+        return this;
+    }
+    incScore(){
+        this.score += 1;
+        console.log(`${this.username} has a score of ${this.score}`);
+        return this;
+    }
+}
+
+class Admin extends User{
+    constructor(username, email, title){
+        super(username, email)
+        this.title = title;
+    }
+    deleteUser(user){
+        users = users.filter((u) => {
+            return u.username !== user.username
+        })
+    }
+}
+
+const userOne = new User('mario', 'mario@thenetninja.co.uk');
+const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
+const userThree = new Admin('shaun','shaun@thenetninja.co.uk', 'black-belt-ninja');
+
+console.log(userThree)
